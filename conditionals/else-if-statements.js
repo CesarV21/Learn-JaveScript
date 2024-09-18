8e1646c8b7b56206fec0d0d034b94a4c33fba4fb
César Jr Villarreal M <cesarjr1406@icloud.com>
2024-09-17 21:21:00 -0500

    Add conditional statements to handle different seasons with corresponding messages

--- conditionals/else-if-statements.js
+++ conditionals/else-if-statements.js
@@ -24,3 +24,30 @@ The else if statements allow you to have multiple possible outcomes. if/else if/
 
 In the example above, since stopLight === 'red' evaluates to false and stopLight === 'yellow' evaluates to true, the code inside the first else if statement is executed. The rest of the conditions are not evaluated. If none of the conditions evaluated to true, then the code in the else statement would have executed.
  */
+
+// ------------------ Instructions  ------------------//
+
+// 1 - Let’s create a program that keeps track of the way the environment changes with the seasons. Write a conditional statement to make this happen!
+// In main.js there is already an if...else statement in place. Let’s add an else if statement that checks if season is equal to 'winter'.
+// Inside the code block of the else if statement, add a console.log() that prints the string 'It\'s winter! Everything is covered in snow.'.
+// Make sure that the structure uses if, else if, and else.
+
+// 2 -Add another else if statement that checks if season is equal to 'fall'.
+// Inside the code block of the else if statement you just created, add a console.log() that prints the string 'It\'s fall! Leaves are falling!'.
+
+// 3 - Add a final else if statement that checks if season is equal to 'summer'.
+// Inside the code block of the else if statement you just created, add a console.log() that prints the string 'It\'s sunny and warm because it\'s summer!'.
+
+let season = "summer";
+
+if (season === "spring") {
+  console.log("It's spring! The trees are budding!");
+} else if (season === "winter") {
+  console.log("It's winter! Everything is covered in snow.");
+} else if (season === "fall") {
+  console.log("It's fall! Leaves are falling!");
+} else if (season === "summer") {
+  console.log("It's sunny and warm because it's summer!");
+} else {
+  console.log("Invalid season.");
+}
